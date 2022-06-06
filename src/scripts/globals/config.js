ce692e0225f27f65de4e458f4f13983b492cd865
mraihanhatim <raihan04042001@gmail.com>
2022-06-07 00:53:23 +0700

    Add key for api

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '38423f80c48af383ea5949ee4ed54547',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
