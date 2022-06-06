9fb8e8972ec3dd823673dbe82cbbe29e3e544a0d
mraihanhatim <raihan04042001@gmail.com>
2022-06-07 00:51:02 +0700

    showing like button in detail page

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -20,28 +20,46 @@ const createMovieDetailTemplate = (movie) => `
     <h3>Overview</h3>
     <p>${movie.overview}</p>
   </div>
+</div>
 `;
 
 const createMovieItemTemplate = (movie) => `
   <div class="movie-item">
     <div class="movie-item__header">
-      <img class="movie-item__header__poster" alt="${movie.title}"
-           src="${
-             movie.backdrop_path
-               ? CONFIG.BASE_IMAGE_URL + movie.backdrop_path
-               : 'https://picsum.photos/id/666/800/450?grayscale'
-           }">
-      <div class="movie-item__header__rating">
-        <p>⭐️<span class="movie-item__header__rating__score">${
-          movie.vote_average
-        }</span></p>
-      </div>
+        <img class="movie-item__header__poster" alt="${movie.title}"
+            src="${
+              movie.backdrop_path
+                ? CONFIG.BASE_IMAGE_URL + movie.backdrop_path
+                : 'https://picsum.photos/id/666/800/450?grayscale'
+            }">
+        <div class="movie-item__header__rating">
+            <p>⭐️<span class="movie-item__header__rating__score">${
+              movie.vote_average
+            }</span></p>
+        </div>
     </div>
     <div class="movie-item__content">
-      <h3><a href="${`/#/detail/${movie.id}`}">${movie.title}</a></h3>
-      <p>${movie.overview}</p>
+        <h3><a href="${`/#/detail/${movie.id}`}">${movie.title}</a></h3>
+        <p>${movie.overview}</p>
     </div>
   </div>
+  `;
+
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this movie" id="likeButton" class="like">
+     <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
 `;
 
-export { createMovieItemTemplate, createMovieDetailTemplate };
+const createLikedButtonTemplate = () => `
+  <button aria-label="unlike this movie" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
+export {
+  createMovieItemTemplate,
+  createMovieDetailTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
+};
